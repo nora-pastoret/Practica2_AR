248891fd00bb1c7f898e187dfc30149398b6744a
nora-pastoret <nora.pastoret@estudiantat.upc.edu>
2025-04-18 14:57:18 +0200

    petits arreglos

--- Assets/PRACTICA2/Scripts/RotateFish.cs
+++ Assets/PRACTICA2/Scripts/RotateFish.cs
@@ -1,26 +1,3 @@
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RotateFish : MonoBehaviour
-{
-    public float velocidadDeRotacion = 20f;
-
-    void Update()
-    {
-
-       if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
-        {
-            Vector2 mov = Input.GetTouch(0).deltaPosition;
-            float x = mov.x * Mathf.Deg2Rad * velocidadDeRotacion;
-            float y = mov.y * Mathf.Deg2Rad * velocidadDeRotacion;
-            transform.RotateAround(Vector3.zero, Vector3.up, -x);
-            transform.RotateAround(Vector3.zero, Vector3.up, y);
-
-        }
-    }
-}*/
-
 using UnityEngine;
 using UnityEngine.InputSystem; // Nou sistema d'input
 
